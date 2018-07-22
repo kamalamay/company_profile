fadf97edd189326ea56e7372533d44b3160ac56d
amay.ikhw <amay.ikhw@gmail.com>
2018-07-22 14:38:34 +0700

    init pertama

---
+++ src/component/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Card extends React.Component{
+    render(){
+        return(
+            <div class="card" >
+            <img class="card-img-top" src="..." alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+
+
+
+
+
+
+        )
+    }
+}
+
+export default Card;
